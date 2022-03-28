269ca5856343e4d9664559f8b7035df1a15ff527
PhamBinh <binhphfx11493@funix.edu.vn>
2022-03-29 04:42:05 +0700

    Combining Reducers

--- src/Componnent/DishdetailComponent .js
+++ src/Componnent/DishdetailComponent .js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem,Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
    function RenderDish({dish}) {
@@ -18,6 +18,7 @@ import { Link } from 'react-router-dom';
     }
 
    function RenderComments({comments}) {
+	  
         if(comments != null){
             return(
                 <div className="col-12 col-md-5 m-1">
